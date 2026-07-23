d37d82c6fac2240500cb32ce4cdd4b2fbbf4fea2
Melollo4 <melvin.piirimets@elev.ga.lbs.se>
2026-07-23 14:20:47 +0200

    testing
    added comment to the single script because hackatime doesn't seem to work

--- Clicker/Assets/Technical/Scripts/Button.cs
+++ Clicker/Assets/Technical/Scripts/Button.cs
@@ -7,7 +7,7 @@ public class Button : MonoBehaviour
     // just for keeping track of basic UI buttons
 
 
-
+    // for keeping track of what button is what and possibly having another script reading methods sending them the buttonName
     public string buttonName;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
