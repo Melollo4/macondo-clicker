c063fd52ce4d5f2bfa74c5be055b1fc4289f7e19
Melollo4 <melvin.regular.human@gmail.com>
2026-07-23 14:02:10 +0200

    First steps
    Added a button script and briefly explained its purpose and added a variable

---
+++ Clicker/Assets/Technical/Scripts/Button.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    // just for keeping track of basic UI buttons
+
+
+
+    public string buttonName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
